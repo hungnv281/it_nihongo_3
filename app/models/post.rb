048d15d682f9381ad87f34c458b9dcde2f58bb2d
hungnv281 <hung.nv281@gmail.com>
2021-12-12 04:44:13 +0000

    add code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
+
 class Post < ApplicationRecord
-	has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy 
+  # dependent: :destroy means the comments related 
+  # to the specific post in mention get deleted if the post does.
 end
